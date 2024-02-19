d653a3f23c64148c70057f97f5f05ecf37290e6b
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-02-19 14:17:09 -0600

    Merge Trees problem solved

---
+++ Python/merge-trees.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        Uses DFS specifically preorder traversal to merge trees by adding similarly positioned nodes
+        and adding nodes that don't exist in the other tree at that position
+        """
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        m_root = TreeNode(root1.val + root2.val)
+        m_root.left = self.mergeTrees(root1.left, root2.left)
+        m_root.right = self.mergeTrees(root1.right, root2.right)
+        
+        return m_root
